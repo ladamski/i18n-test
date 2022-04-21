391a04bdca71c5e9f20a5a275bfb77fdc8d20fab
ladamski@duckduckgo.com <ladamski@duckduckgo.com>
2022-04-20 21:46:36 -0700

    i18n with icu

--- i18next.js
+++ i18next.js
@@ -1,28 +1,36 @@
 import i18next from 'i18next';
+// import Backend from 'i18next-http-backend';
+import ICU from "i18next-icu";
 
 // plurals, see https://www.i18next.com/translation-function/plurals
-i18next.init({
+i18next
+    .use(ICU)
+    // .use(Backend)
+    .init({
     lng: 'en', // if you're using a language detector, do not define the lng option
     debug: false,
     resources: {
         en: {
             translation: {
-                "tab_one": "Close one tab",
-                "tab_other": "Close {{count}} tabs"
+                tab: `Close {count, plural,
+one {# tab}
+other {# tabs}
+}`            
             }            
         },
         pl: {
             translation: {
-                "tab_one": "Zamknij kartę",
-                "tab_few": "Zamknij {{count}} karty", // applies to some languages like PL that support multiple plurals, not EN
-                "tab_many": "Zamknij {{count}} kart",
+                tab: `Zamknij {count, plural,
+one {kartę}
+few {# karty}
+other {# kart}
+}`
             }            
         },
     }
 });
 
 // initialized and ready to go!
-//  document.getElementById('output').innerHTML = i18next.t('key');
 console.log('** EN')
 console.log(i18next.t('tab', {count: 1}) );
 console.log(i18next.t('tab', {count: 3}) );
@@ -31,4 +39,4 @@ i18next.changeLanguage('pl')
 console.log('** PL')
 console.log(i18next.t('tab', {count: 1}) );
 console.log(i18next.t('tab', {count: 3}) );
-console.log(i18next.t('tab', {count: 500}) );
+console.log(i18next.t('tab', {count: 500}), 'comment' );
