67db44084bbe8fae7b32f9f55c3a2314b82b001b
ladamski@duckduckgo.com <ladamski@duckduckgo.com>
2022-04-22 15:14:16 -0700

    update to json icu format

--- i18next.js
+++ i18next.js
@@ -1,42 +1,32 @@
 import i18next from 'i18next';
-// import Backend from 'i18next-http-backend';
+import Backend from 'i18next-fs-backend';
 import ICU from "i18next-icu";
+import { join } from "path"
+import { readdirSync, lstatSync } from "fs"
 
 // plurals, see https://www.i18next.com/translation-function/plurals
 i18next
     .use(ICU)
-    // .use(Backend)
+    .use(Backend)
     .init({
-    lng: 'en', // if you're using a language detector, do not define the lng option
-    debug: false,
-    resources: {
-        en: {
-            translation: {
-                tab: `Close {count, plural,
-one {# tab}
-other {# tabs}
-}`            
-            }            
+        // debug: true,
+        initImmediate: false,
+        fallbackLng: 'en',
+        lng: 'en',
+        // ns: 'backend-app',
+        // defaultNS: 'backend-app',
+        backend: {
+          loadPath: join(".", './locales/{{lng}}/{{lng}}.json'),
         },
-        pl: {
-            translation: {
-                tab: `Zamknij {count, plural,
-one {kartę}
-few {# karty}
-other {# kart}
-}`
-            }            
-        },
-    }
-});
+    })
 
 // initialized and ready to go!
 console.log('** EN')
-console.log(i18next.t('tab', {count: 1}) );
-console.log(i18next.t('tab', {count: 3}) );
-console.log(i18next.t('tab', {count: 500}) );
+console.log(i18next.t('tab.title', {count: 1}) );
+console.log(i18next.t('tab.title', {count: 3}) );
+console.log(i18next.t('tab.title', {count: 500}) );
 i18next.changeLanguage('pl')
 console.log('** PL')
-console.log(i18next.t('tab', {count: 1}) );
-console.log(i18next.t('tab', {count: 3}) );
-console.log(i18next.t('tab', {count: 500}), 'comment' );
+console.log(i18next.t('tab.title', {count: 1}) );
+console.log(i18next.t('tab.title', {count: 3}) );
+console.log(i18next.t('tab.title', {count: 500}) );
